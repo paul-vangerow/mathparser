dd3a9057e7920b6d6a95f4076850b9a4272b91e7
Mineglitch <pavangerow@gmail.com>
2025-04-06 15:08:21 +0100

    This currently works

--- src/parser/parser.h
+++ src/parser/parser.h
@@ -49,37 +49,44 @@ public:
 
     void parse_token_subset(std::vector<std::unique_ptr<Token>>& in){
         // Iterate over each rule
-        for (auto rule_it = m_rule_set.rbegin(); rule_it != m_rule_set.rend(); ++rule_it){
-            auto rule_match_string = rule_it->match_string;
-            auto rule_match_length = get_rule_length(rule_match_string);
-            auto rule_make_fn = rule_it->make_function; 
+        bool modified = true;
+        while (modified){
+            modified = false;
+            for (auto rule_it = m_rule_set.rbegin(); rule_it != m_rule_set.rend(); ++rule_it){
+                auto rule_match_string = rule_it->match_string;
+                auto rule_match_length = get_rule_length(rule_match_string);
+                auto rule_make_fn = rule_it->make_function; 
 
-            for (std::size_t input_it = 0; input_it + rule_match_length - 1 < in.size(); ++input_it){
-                std::string match_check;
-                for (std::size_t i = 0; i < rule_match_length; i++){
-                    std::cout << in.size() << " " << (input_it + i) << "\n";
-                    if (!in[input_it + i]){
-                        std::cout << "Nullptr access\n";
-                        continue; 
-                    }
-                    match_check += in[input_it + i]->get_dtype();
-                    if (i != rule_match_length-1) match_check += " ";
-                }
-                std::cout << match_check << "<< || >>" << rule_match_string << "\n";
-                if (match_check == rule_match_string){
-                    std::cout << "Matched!\n";
-                    std::vector<std::unique_ptr<Token>> convert;
+                // Iterate over all tokens
+                for (std::size_t input_it = 0; input_it + rule_match_length - 1 < in.size(); ++input_it){
+                    std::string match_check;
+                    // Get the match string of the indexed tokens
                     for (std::size_t i = 0; i < rule_match_length; i++){
-                        convert.push_back(std::move(in[input_it + i]));
+                        if (!in[input_it + i]){
+                            continue; 
+                        }
+                        match_check += in[input_it + i]->get_dtype();
+                        if (i != rule_match_length-1) match_check += " ";
                     }
-                    auto new_token = rule_make_fn(std::move(convert));
-                    in[input_it] = std::move(new_token);
-                    if (rule_match_length > 1){
-                        in.erase(in.begin() + input_it + 1, in.begin() + input_it + rule_match_length);
+                    // If the match string matches the rule, extract the tokens, build
+                    // the new Token and erase the other tokens.
+                    if (match_check == rule_match_string){
+                        std::vector<std::unique_ptr<Token>> convert;
+                        for (std::size_t i = 0; i < rule_match_length; i++){
+                            convert.push_back(std::move(in[input_it + i]));
+                        }
+                        auto new_token = rule_make_fn(std::move(convert));
+                        in[input_it] = std::move(new_token);
+                        if (rule_match_length > 1){
+                            in.erase(in.begin() + input_it + 1, in.begin() + input_it + rule_match_length);
+                        }
+                        modified = true;
                     }
                 }
             }
         }
+        std::cout << in.size() << "\n";
+        in[0]->print(std::cout);
     }
 
     std::size_t get_rule_length(std::string match){
